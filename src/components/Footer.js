6c4a26446758ee29e2f6723599403f0cd972ef86
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2024-10-22 14:17:26 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,25 @@
 import React from 'react';
-import { Box, Typography, Link, Grid } from '@mui/material';
+import { Box, Typography, Link, Grid, IconButton } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+//npm install @mui/icons-material instaliuoti reikia
 
 export default function Footer() {
   const { t } = useTranslation(); // Kalbų vertimų hooks
 
   return (
-    <Box
-      component="footer"
-      sx={{
-        py: 2,
-        px: 4,
-        mt: 'auto',
-        backgroundColor: (theme) => theme.palette.footer.background, // Naudojame temoje apibrėžtą spalvą
-        color: (theme) => theme.palette.footer.text, // Naudojame temoje apibrėžtą spalvą
-      }}
-    >
-      <Grid container spacing={2} justifyContent="space-between">
+    <StyledBox component="footer">
+      <Grid container spacing={2} justifyContent="space-between" alignItems="center">
         <Grid item>
           <Typography variant="body1">
             &copy; {new Date().getFullYear()} {t('footer.copyright')}
           </Typography>
         </Grid>
         <Grid item>
-          <Box sx={{ display: 'flex', gap: 2 }}>
+          <PageLinksBox>
             <Link href="#about" color="inherit" underline="none">
               {t('footer.about')}
             </Link>
@@ -35,7 +31,38 @@ export default function Footer() {
             </Link>
           </Box>
         </Grid>
+        <Grid item>
+          <SocialMediaBox>
+            <IconButton color="inherit" component="a" href="https://www.facebook.com/gabriele.tamaseviciute" target="_blank" rel="noopener noreferrer">
+              <FacebookIcon />
+            </IconButton>
+            <IconButton color="inherit" component="a" href="https://www.linkedin.com/in/gabrielė-tamaševičiūtė-06712526b" target="_blank" rel="noopener noreferrer">
+              <LinkedInIcon />
+            </IconButton>
+            <IconButton color="inherit" component="a" href="https://github.com/gabwowce" target="_blank" rel="noopener noreferrer">
+              <GitHubIcon />
+            </IconButton>
+          </Box>
+        </Grid>
       </Grid>
     </Box>
   );
 }
+
+const SocialMediaBox = styled(Box)(({ theme }) => ({
+  display: 'flex', 
+  gap: 1
+}));
+
+const PageLinksBox = styled(Box)(({ theme }) => ({
+  display: 'flex', 
+  gap: 2
+}));
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  py: 2,
+  px: 4,
+  mt: 'auto',
+  backgroundColor: (theme) => theme.palette.footer.background, 
+  color: (theme) => theme.palette.footer.text,
+}));
