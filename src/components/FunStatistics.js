2ce9cf780fcde1748c2ebcd3460c23d76c82a59f
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2024-10-30 12:55:10 +0200

    Update FunStatistics.js

--- src/components/FunStatistics.js
+++ src/components/FunStatistics.js
@@ -4,11 +4,19 @@ import { useTranslation } from 'react-i18next';
 import { styled, useTheme } from '@mui/material/styles';
 import { ThemeContext } from '../context/ThemeContext'; 
 
+import { slideInRightAnimation} from '../styles/animations';
+import { useVisibility } from '../context/VisibilityContext';
+
+import CodeIcon from '@mui/icons-material/Code';
+import ErrorIcon from '@mui/icons-material/Error';
+import BugReportIcon from '@mui/icons-material/BugReport';
+import MonitorIcon from '@mui/icons-material/Monitor';
+
 const statisticsData = [
-    { key: 'linesOfCode' },
-    { key: 'codeFirstTry' },
-    { key: 'bugsSquashed' },
-    { key: 'monitorsUsed' },
+    { key: 'linesOfCode', icon: CodeIcon},
+    { key: 'codeFirstTry', icon: ErrorIcon},
+    { key: 'bugsSquashed', icon: BugReportIcon},
+    { key: 'monitorsUsed', icon:  MonitorIcon},
 ];
 
 const Statistics = () => {
@@ -16,27 +24,45 @@ const Statistics = () => {
     const { themeMode } = useContext(ThemeContext); 
     const theme = useTheme();
 
+     const [animate, setAnimate] = useState(false);
+    useEffect(() => {
+      const isStatsVisible = visibleElements.has("about-stats");
+      isStatsVisible && setAnimate(true);
+    }, [visibleElements]);
+
     return (
-        <StyledBox>
+        <StyledBox id="about-stats" className="track-visibility">
                 <NameTypography variant="h2" animate={animate}>
                     {t('aboutPage.stats.title')}
                   </NameTypography>
                   
-                <StyledBox>
-                    {statisticsData.map((stat) => (
-                        <StyledCard key={stat.key}>
+                <StyledBox >
+                    {statisticsData.map((stat, index) => (
+                        const IconComponent = stat.icon;
+    
+                        <StyledCard key={stat.key} animate={animate} index={index}>
                             <CardContent>
+                                 <StyledIconComponent/>
                                 <Typography variant="h6">{t(`statistics.${stat.key}.title`)}</Typography>
                                 <Typography variant="body1">{t(`statistics.${stat.key}.number`)}</Typography>
                             </CardContent>
                         </StyledCard>
                     ))}
-                </StyledBox>
+                </StyledBox2>
         </StyledBox>
     );
 };
 
 export default Statistics;
+
+const StyledIconComponent = styled(IconComponent)(({ theme }) => ({
+    fontSize="large",
+    color="primary",
+    position:'absolute',
+    left:'50%',
+    top:"-50%",
+  }));
+
 
 const NameTypography = styled(Typography)(({ theme, animate }) => ({
     fontFamily: 'Outfit, sans-serif',
@@ -48,17 +74,21 @@ const NameTypography = styled(Typography)(({ theme, animate }) => ({
   }));
 
 
-const StyledCard = styled(Card)(({ theme }) => ({
+const StyledCard = styled(Card)(({ theme, animate, index }) => ({
   mb: 2, 
-  width: 300, 
-  textAlign: 'center'
- 
+  width: '100%', 
+  textAlign: 'center',
+  animation: animate && `${slideInLeftAnimation} 2.5s ease forwards`,
+  animationDelay: animate && `${index * 0.3}s`,
+  position:'relative',
+  borderTop: `2px solid gray`, 
+  paddingTop: '20px',
 }));  
 
 const StyledBox = styled(Box)(({ theme }) => ({
   display: 'flex',
-  flexDirection: 'column',
+  flexDirection: 'row',
   alignItems: 'center',
-  mt: 4,
+  width:'100%'
  
 }));  
